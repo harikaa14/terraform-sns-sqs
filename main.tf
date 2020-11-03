a63ae8131e49ee60c2027e8e4c39fa50a515e1d2
harikaa14 <72727077+harikaa14@users.noreply.github.com>
2020-11-03 17:48:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,5 +28,5 @@ EOF
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.email_notification.arn
   protocol  = "sqs"
-  endpoint  = aws_sqs_queue.user_updates_queue.arn
+  endpoint  = var.sqs_endpoint_arn
 }
