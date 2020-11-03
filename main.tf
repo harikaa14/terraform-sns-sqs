072563412187b5582e505736216cba2db98bcb71
harikaa14 <72727077+harikaa14@users.noreply.github.com>
2020-11-03 17:19:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,26 @@ provider "aws" {
 
 resource "aws_sns_topic" "email_notification" {
   name = "user-updates-topic"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
 }
-
+EOF
+}
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.email_notification.arn
